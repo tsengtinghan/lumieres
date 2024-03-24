dcdb5a654a70c60815eea8402e78452d7dc4808d
Michael Tseng <michael08120@gmail.com>
2024-03-24 08:25:09 -0700

    added cahced get request

--- components/youtube-player.tsx
+++ components/youtube-player.tsx
@@ -24,6 +24,12 @@ interface QuestionList {
   question: Question;
 }
 
+interface CachedGeneration {
+  title: string;
+  url: string;
+  questions: { question: Question; timestamp: number }[];
+}
+
 function getIdfromUrl(url: string): string | null {
   const urlParams = new URLSearchParams(url.split("?")[1]);
   return urlParams.get("v");
@@ -39,7 +45,6 @@ type ButtonVariant =
   | null
   | undefined;
 
-
 const YoutubePlayer: React.FC = () => {
   const [questions, setQuestions] = useState<QuestionList[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -50,10 +55,24 @@ const YoutubePlayer: React.FC = () => {
   const playerRef = useRef<any>(null);
   const [selectedOption, setSelectedOption] = useState("");
   const lastVideo = useRef(false);
-  const cached_urls = [
-    'https://www.youtube.com/watch?v=5qap5aO4i9A', 'https://www.youtube.com/watch?v=zjkBMFhNj_g&t=758s'
-  ]
-  
+  const [cached_urls, setCachedUrls] = useState<CachedGeneration[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ShinnosukeUesaka/lumieres_backend/main/cached_generations.json"
+    )
+      .then((response) => response.json())
+      .then((data: { cached_generations: CachedGeneration[] }) => {
+        // Ensure data.cached_generations is an array
+        if (Array.isArray(data.cached_generations)) {
+          setCachedUrls(data.cached_generations);
+        } else {
+          console.error("Fetched data is not an array");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   const opts = {
     width: "100%",
     height: "570px",
@@ -65,19 +84,19 @@ const YoutubePlayer: React.FC = () => {
   const questionList: QuestionList[] = test;
 
   const startVideo = () => {
-    loadingState === ("waiting_for_url") &&
+    loadingState === "waiting_for_url" &&
       setLoadingState("waiting_for_backend");
     // POST
     // https://lumieres-backend.onrender.com/create_questions
     //{"url":"https://www.youtube.com/watch?v=zjkBMFhNj_g", "max_questions":1}
     axios
-    .post(
+      .post(
         "https://lumieres-backend.onrender.com/demo/create_questions",
         {
           url: mainVideoUrl,
           max_questions: 1,
         },
-        { timeout: 600000 } 
+        { timeout: 600000 }
       )
       .then((response) => {
         console.log(response);
@@ -140,110 +159,109 @@ const YoutubePlayer: React.FC = () => {
         questions[currentQuestionIndex].question.correct_answer_video_url
       );
       setSelectedOption(choice);
-        lastVideo.current = true;
+      lastVideo.current = true;
     } else {
       setVideoUrl(
         questions[currentQuestionIndex].question.wrong_answer_video_url
       );
       setSelectedOption(choice);
-        lastVideo.current = true;
+      lastVideo.current = true;
     }
     // setShowQuestion(false);
     // playerRef.current.playVideo();
   };
-
 
   const loadingScreen = (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Loading...</h1>
     </div>
   );
-  const handleVideoEnd = () => {    
+  const handleVideoEnd = () => {
     console.log("Video Ended");
     console.log(lastVideo.current);
-    if(lastVideo.current){
-        setShowQuestion(false);
-        setSelectedOption("")
-        lastVideo.current = false;
-        playerRef.current.playVideo();
+    if (lastVideo.current) {
+      setShowQuestion(false);
+      setSelectedOption("");
+      lastVideo.current = false;
+      playerRef.current.playVideo();
     }
-  }
+  };
 
-  const youtubeComponent = (<YouTube
-    videoId={getIdfromUrl(mainVideoUrl) as string} // video id
-    opts={opts}
-    onStateChange={videoStateChange}/>);
+  const youtubeComponent = (
+    <YouTube
+      videoId={getIdfromUrl(mainVideoUrl) as string} // video id
+      opts={opts}
+      onStateChange={videoStateChange}
+    />
+  );
 
   let mainComponent;
   if (showQuestion) {
-
     mainComponent = (
       <div className="grid grid-cols-2 gap-4 w-full h-full p-10">
         <div className="flex w-full h-full items-center justify-center">
-          <video 
-                  src={videoUrl} 
-                  className="w-[250px] h-[250px] rounded-full border-none"
-                  onEnded={handleVideoEnd}
-                  autoPlay
+          <video
+            src={videoUrl}
+            className="w-[250px] h-[250px] rounded-full border-none"
+            onEnded={handleVideoEnd}
+            autoPlay
           />
         </div>
 
-          
-          <div className="flex flex-col space-y-3 justify-center p-4 h-full">
-            <h1 className="text-xl mb-3">{questions[currentQuestionIndex].question.question}</h1>
-            {questions[currentQuestionIndex].question.options?.map(
-              (item, index) => {
-                if (
-                  item === selectedOption &&
-                  item !== questions[currentQuestionIndex].question.answer
-                ) {
-                  return (
-                    <Button
-                      key={index}
-                      onClick={() => handleClick(item)}
-                      variant="destructive"
-                      className="!text-left justify-start text-lg h-16"
-                    >
-                      {item}
-                    </Button>
-                  );
-                } else if (
-                  item === selectedOption &&
-                  item === questions[currentQuestionIndex].question.answer
-                ) {
-                  return (
-                    <Button
-                      key={index}
-                      onClick={() => handleClick(item)}
-                      variant="secondary"
-                      className="!text-left justify-start text-lg h-16"
-                    >
-                      {item}
-                    </Button>
-                  );
-                }
+        <div className="flex flex-col space-y-3 justify-center p-4 h-full">
+          <h1 className="text-xl mb-3">
+            {questions[currentQuestionIndex].question.question}
+          </h1>
+          {questions[currentQuestionIndex].question.options?.map(
+            (item, index) => {
+              if (
+                item === selectedOption &&
+                item !== questions[currentQuestionIndex].question.answer
+              ) {
                 return (
                   <Button
                     key={index}
                     onClick={() => handleClick(item)}
-                    variant="outline"
+                    variant="destructive"
+                    className="!text-left justify-start text-lg h-16"
+                  >
+                    {item}
+                  </Button>
+                );
+              } else if (
+                item === selectedOption &&
+                item === questions[currentQuestionIndex].question.answer
+              ) {
+                return (
+                  <Button
+                    key={index}
+                    onClick={() => handleClick(item)}
+                    variant="secondary"
                     className="!text-left justify-start text-lg h-16"
                   >
                     {item}
                   </Button>
                 );
               }
-            )}
-          </div>
-
+              return (
+                <Button
+                  key={index}
+                  onClick={() => handleClick(item)}
+                  variant="outline"
+                  className="!text-left justify-start text-lg h-16"
+                >
+                  {item}
+                </Button>
+              );
+            }
+          )}
+        </div>
       </div>
     );
   } else if (loadingState === "waiting_for_url") {
     mainComponent = (
       <div className="h-full w-full flex justify-center items-center flex-col">
-        <div className="text-5xl text-center align-middle p-12">
-          Lumiere
-        </div>
+        <div className="text-5xl text-center align-middle p-12">Lumiere</div>
         <div>Please keep your sound on</div>
       </div>
     );
@@ -251,95 +269,96 @@ const YoutubePlayer: React.FC = () => {
     mainComponent = (
       <div className="flex flex-col items-center h-full w-full p-12">
         <div className="w-full h-full">
-          <div className="loader-inner">
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-          </div>
+          <iframe
+            src="https://lottie.host/embed/d16e4fcb-fddd-44b8-9ced-18606864327b/qhZRJNqBpV.json"
+            className="w-full h-full"
+          ></iframe>
+          <h1 className="bottom-3/4">Generation takes up to 3 minutes.</h1>
         </div>
-        <h1 className="bottom-3/4">Generation takes up to 3 minutes.</h1>
       </div>
     );
   } else if (loadingState === "questions_loaded") {
-    mainComponent = (
-      <></>
-    );
+    mainComponent = <></>;
   }
-  console.log(loadingState)
-  console.log(showQuestion)
+  console.log(loadingState);
+  console.log(showQuestion);
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center gap-4 p-2  bg-orange-100 overflow-visible ">
       <div className="w-[100%] bg-white max-w-screen-lg shadow-2xl rounded-2xl aspect-[16/9] m-12 mb-16 flex">
-        <div className={"w-full h-full bg-white z-0"} >
-          {mainComponent}
-        </div>
-        <div className={"w-full h-full ml-[-100%] z-10 " + (loadingState === "questions_loaded" && !showQuestion  ? 'visible' : 'invisible' )}>
+        <div className={"w-full h-full bg-white z-0"}>{mainComponent}</div>
+        <div
+          className={
+            "w-full h-full ml-[-100%] z-10 " +
+            (loadingState === "questions_loaded" && !showQuestion
+              ? "visible"
+              : "invisible")
+          }
+        >
           <YouTube
-          videoId={getIdfromUrl(mainVideoUrl) as string} // video id
-          opts={opts}
-          onStateChange={videoStateChange}
+            videoId={getIdfromUrl(mainVideoUrl) as string} // video id
+            opts={opts}
+            onStateChange={videoStateChange}
           />
         </div>
       </div>
-      <div className={"w-full max-w-lg flex justify-between items-center relative -top-12 gap-4 bg-black rounded-3xl p-2 " + ( loadingState === "waiting_for_url" ? 'visible' : 'invisible' )}>
-        {cached_urls.map((url) => {
-            return (
+      <div
+        className={"flex flex-col" + (loadingState === "waiting_for_url" ? "visible" : "invisible")}
+      >
+        {cached_urls.map((item) => {
+          return (
             <Button
-                onClick={() => {
-                setMainVideoUrl(url);
+              onClick={() => {
+                setMainVideoUrl(item.url);
                 startVideo();
-                }}
+              }}
+              className="rounded-3xl"
+              variant={"outline"}
             >
-                {url}
+              {item.title}
             </Button>
-            );
+          );
         })}
-        <Label className="sr-only" htmlFor="input-field">
-          Input Field
-        </Label>
-        <Input
-          className="flex-grow text-white bg-black placeholder-white border-0 rounded-3xl focus:outline-none focus-visible:ring-offset-0 focus:ring-0"
-          id="input-field"
-          placeholder="Enter Link Here"
-          onChange={(e) => setMainVideoUrl(e.target.value)}
-        />
-        <Button
-          className="bg-white text-black p-2 rounded-3xl"
-          onClick={startVideo}
+        <div
+          className={
+            "w-full max-w-lg flex justify-between items-center relative -top-12 gap-4 bg-black rounded-3xl p-2 " +
+            (loadingState === "waiting_for_url" ? "visible" : "invisible")
+          }
         >
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+          <Label className="sr-only" htmlFor="input-field">
+            Input Field
+          </Label>
+          <Input
+            className="flex-grow text-white bg-black placeholder-white border-0 rounded-3xl focus:outline-none focus-visible:ring-offset-0 focus:ring-0"
+            id="input-field"
+            placeholder="Enter Link Here"
+            onChange={(e) => setMainVideoUrl(e.target.value)}
+          />
+          <Button
+            className="bg-white text-black p-2 rounded-3xl"
+            onClick={startVideo}
           >
-            <path
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-            />
-          </svg>
-        </Button>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+              />
+            </svg>
+          </Button>
+        </div>
       </div>
-      <div className="absolute top-2 left-0 -mt-16 -ml-16 w-40 h-40 bg-red-500 rounded-full opacity-50 shadow-lg" ></div>
+      <div className="absolute top-2 left-0 -mt-16 -ml-16 w-40 h-40 bg-red-500 rounded-full opacity-50 shadow-lg"></div>
       <div className="absolute top-2 left-16 -mt-10 w-20 h-20 bg-orange-400 rounded-full opacity-50 shadow-lg"></div>
       <div className="absolute top-5 left-14 -mt-2 -ml-2 w-20 h-20 bg-yellow-400 rounded-full opacity-50 shadow-lg"></div>
-      <div className="absolute bottom-10 right-0 -mb-16 -mr-16 w-40 h-40 bg-red-500 rounded-full opacity-50 shadow-lg" ></div>
+      <div className="absolute bottom-10 right-0 -mb-16 -mr-16 w-40 h-40 bg-red-500 rounded-full opacity-50 shadow-lg"></div>
       <div className="absolute bottom-2 right-10 -mb-10 ml-5 w-20 h-20 bg-orange-400 rounded-full opacity-50 shadow-lg"></div>
       <div className="absolute bottom-5 right-14 -mb-2 -mr-2 w-20 h-20 bg-yellow-500 rounded-full opacity-50 shadow-lg"></div>
     </div>
